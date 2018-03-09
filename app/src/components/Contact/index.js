ba2edb3183fef0697d4ddbbec0e2d0322d88c926
Pauline Ballot <ballotp@gmail.com>
2018-03-09 11:16:17 +0100

    escape cross

--- app/src/components/Contact/index.js
+++ app/src/components/Contact/index.js
@@ -2,7 +2,8 @@
  * Npm import
  */
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 /**
  * Local import
  */
@@ -13,10 +14,16 @@ import React from 'react';
 
 const Contact = () => (
   <div id="mentionscontact">
+    <Link
+      to="/"
+      className="escape-link"
+    >
+      <div><FontAwesomeIcon className="escape" icon="times" /></div>
+    </Link>
     <h1>Contact</h1>
     <p>
-      Envie de nous faire le retour de ton expérience ? Des suggestions pour l'evolution de ton site préféré?
-      Contactes nous à <span className="yellow">matefinder@gmail.com</span> ou clique sur le bouton ci-dessous !
+      Envie de nous faire le retour de ton expérience ? Des suggestions pour l'evolution de ton site préféré ?
+      Contacte-nous à <span className="yellow">matefinder@gmail.com</span> ou clique sur le bouton ci-dessous !
     </p>
     <a id="contactmail" href="mailto:matefinder@gmail.com">Envoyer un email</a>
   </div>
